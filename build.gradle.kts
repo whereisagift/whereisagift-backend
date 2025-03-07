d08c7f4d84bbae58e2d037ca30e2387a2f405318
Eugeny Mazurov <deivoff@gmail.com>
2025-03-07 18:18:53 +0500

    fix: added logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,22 +17,18 @@ repositories {
 	mavenCentral()
 }
 
-
 dependencies {
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	implementation("org.projectlombok:lombok:1.18.36")
 	implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:11.1.0")
 	implementation("com.graphql-java:graphql-java-extended-scalars:22.0")
 	implementation("com.graphql-java-kickstart:graphql-java-tools:11.1.0")
-	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-validation
 	implementation("org.springframework.boot:spring-boot-starter-validation:2.7.4")
-	// https://mvnrepository.com/artifact/jakarta.validation/jakarta.validation-api
-//	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	developmentOnly("org.springframework.boot:spring-boot-devtools") // devtools для разработки
 }
 
 tasks.withType<Test> {
