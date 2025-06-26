0c0a1360e76ffc144a02eca8c8e2fde85a74373d
misha-git-droid <budarin.flip@gmail.com>
2025-06-27 01:28:50 +0500

    feat(core): add updateWishlist

--- src/main/java/com/whereisagift/wishlist/WishlistRepository.java
+++ src/main/java/com/whereisagift/wishlist/WishlistRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
     Iterable<Wishlist> findByCreatorId(Long id);
-    boolean existsByIdAndCreatorId(Long wishlistId, Long creatorId);
 
 }
--- src/main/java/com/whereisagift/wishlist/WishlistService.java
+++ src/main/java/com/whereisagift/wishlist/WishlistService.java
@@ -1,27 +1,71 @@
 package com.whereisagift.wishlist;
 
+import com.whereisagift.user.UserRepository;
+import com.whereisagift.wish.Wish;
+import com.whereisagift.wish.WishRepository;
+import graphql.GraphQLException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.AccessDeniedException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class WishlistService {
 
     final private WishlistRepository wishlistRepository;
+    final private UserRepository userRepository;
+    final private WishRepository wishRepository;
 
-    public boolean isOwner(Long wishlistId, Long userId) {
-        //проверяем существование вишлиста
-        if (!wishlistRepository.existsById(wishlistId)) {
-            throw new ResourceNotFoundException("Wishlist not found");
+    public Wishlist createWishlist(WishlistInput wishlistInput, Long userId) {
+
+        Wishlist wishlist = new Wishlist();
+        wishlist.setName(wishlistInput.getName());
+        wishlist.setDescription(wishlistInput.getDescription());
+        wishlist.setCreator(userRepository.getReferenceById(userId));
+//        wishlist.setCreatedAt(OffsetDateTime.now());
+//        wishlist.setUpdatedAt(OffsetDateTime.now());
+
+        if (!wishlistInput.getWishIds().isEmpty()) {
+
+            List<Long> wishIds = wishlistInput.getWishIds().stream()
+                    .map(Long::valueOf)
+                    .collect(Collectors.toList());
+
+            List<Wish> wishes = wishRepository.findAllById(wishIds);
+            wishlist.setWishes(wishes);
         }
 
-        //проверяем владельца
-        if (!wishlistRepository.existsByIdAndCreatorId(wishlistId, userId)) {
-            throw new AccessDeniedException("You dont have permission to edit this wishlist");
+        return wishlistRepository.save(wishlist);
+    }
+
+    public Wishlist updateWishlist(Long id, WishlistInput wishlistInput, Long userId) {
+
+        Wishlist wishlist = wishlistRepository.findById(id).orElseThrow(() -> new GraphQLException("Wishlist not found"));
+
+        if (!Objects.equals(wishlist.getCreator().getId(), userId))
+            throw new GraphQLException("Access denied");
+
+        // обновляем поля
+        Optional.ofNullable(wishlistInput.getName()).ifPresent(wishlist::setName);
+        Optional.ofNullable(wishlistInput.getDescription()).ifPresent(wishlist::setDescription);
+
+        // обновляем список желаний
+        if (!wishlistInput.getWishIds().isEmpty()) {
+            List<Wish> wishes = wishRepository.findAllById(
+                    wishlistInput.getWishIds().stream()
+                            .map(Long::valueOf)
+                            .collect(Collectors.toList())
+            );
+            // какие-то желания могу быть не найдены, вернется только часть (без ошибки и исключения)
+            wishlist.setWishes(wishes);
         }
 
-        return true;
+        return wishlistRepository.save(wishlist);
     }
 }
